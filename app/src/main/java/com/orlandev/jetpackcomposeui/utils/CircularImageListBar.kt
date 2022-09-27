af264d53efa08ca6eabb64fe26c87d574a45d1ff
Orlando Novas Rodriguez <38017009+orlandev@users.noreply.github.com>
2022-09-27 13:00:14 -0400

    Change name of file

--- app/src/main/java/com/orlandev/jetpackcomposeui/utils/CircularImageListBar.kt
+++ app/src/main/java/com/orlandev/jetpackcomposeui/utils/CircularImageListBar.kt
@@ -2,18 +2,14 @@ package com.orlandev.jetpackcomposeui.utils
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
